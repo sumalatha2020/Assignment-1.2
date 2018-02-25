52b7ee45d0ebdeb17bc2d7b459e22ec362dc559b
sumalatha2020 <infosumalatha@gmail.com>
2018-02-25 15:26:50 +0530

    Update DivisibilityCheck.py
    Changes mage to include both range values

--- DivisibilityCheck.py
+++ DivisibilityCheck.py
@@ -2,7 +2,7 @@ l=[]  #An array
 
 #loop through the given range to findout numbers divisible by 7 but not 5
 
-for i in range(2000, 3201):
+for i in range(2000, 3200):
     if (i%7==0) and (i%5!=0):
         l.append(str(i))
 
